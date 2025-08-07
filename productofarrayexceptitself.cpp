a89de9ddc65b556c988ef8c73c6716affdcc5b14
sunitipaliwal <paliwalsuniti99@gmail.com>
2025-08-07 22:21:24 +0530

    day9

---
+++ productofarrayexceptitself.cpp
@@ -0,0 +1,24 @@
+
+class Solution
+{
+public:
+  vector<int> productExceptSelf(vector<int> &nums)
+  {
+    int n = nums.size();
+    vector<int> res(n, 1);
+    int pre = 1;
+    for (int i = 0; i < n; i++)
+    {
+      res[i] = pre;
+      pre *= nums[i];
+    }
+    int post = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+      res[i] *= post;
+
+      post *= nums[i];
+    }
+    return res;
+  }
+};
