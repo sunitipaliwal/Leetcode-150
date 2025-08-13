7045841a46c83ca4579ac327aedd71b5c834fbc1
sunitipaliwal <paliwalsuniti99@gmail.com>
2025-08-13 22:32:25 +0530

    day13

---
+++ length-of-lastWord.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+  int lengthOfLastWord(string s)
+  {
+    int length = 0;
+    for (int i = s.size() - 1; i >= 0; i--)
+    {
+      if (s[i] == ' ' && length != 0)
+      {
+        break;
+      }
+      else
+      {
+        if (s[i] != ' ')
+          length++;
+      }
+    }
+    return length;
+  }
+};
